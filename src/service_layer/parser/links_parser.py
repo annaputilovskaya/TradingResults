f55ea6b7094d6028ce1dd7c2f0dbac7c81621f10
anna putilovskaya <putilovskayaam@gmail.com>
2025-04-23 15:35:42 +0300

    create extract_links_from_response function

--- src/service_layer/parser/links_parser.py
+++ src/service_layer/parser/links_parser.py
@@ -1,3 +1,13 @@
+import logging
+
+from aiohttp import ClientSession
+from bs4 import BeautifulSoup
+
+from src.service_layer.utils import get_date_from_link
+
+log = logging.getLogger(__name__)
+
+
 async def get_html(url: str, session: ClientSession) -> str | None:
     """
     Sends a GET request to the given URL and returns the response data.
@@ -16,3 +26,36 @@ async def get_html(url: str, session: ClientSession) -> str | None:
     except Exception as e:
         log.error(f"Error: Failed to fetch data from {url}: {e}")
         raise e
+
+
+def extract_links_from_response(
+    response: str, earliest_date: str, links: set[str], is_new: bool
+) -> tuple[set[str], bool]:
+    """
+    Extracts links to XLS files with daily trading results from the given HTML response.
+
+    Args:
+        response (str): The HTML response containing the trading results.
+        earliest_date (str): The earliest date to consider in the format "YYYYMMDD".
+        links (set[str]): The set of links to add new ones to.
+        is_new (bool): A boolean indicating if new links were found.
+
+    Returns:
+        tuple[set[str], bool]: The updated set of links and a boolean indicating if new links were found.
+    """
+    soup = BeautifulSoup(response, "html.parser")
+    block = soup.find("div", class_="accordeon-inner")
+    items = block.select(
+        "div.accordeon-inner__wrap-item",
+    )
+    for item in items:
+        string = item.find("a")
+        link = string.get("href")
+        if link:
+            date = get_date_from_link(link)
+            if date >= earliest_date:
+                links.add(link)
+            else:
+                is_new = False
+                break
+    return links, is_new
