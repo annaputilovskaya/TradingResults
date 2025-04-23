fa48aba05b29473b62d56c4c09a89dfd7c89596b
anna putilovskaya <putilovskayaam@gmail.com>
2025-04-23 15:48:52 +0300

    create extract_data_from_file function

--- src/service_layer/parser/data_parser.py
+++ src/service_layer/parser/data_parser.py
@@ -1,5 +1,8 @@
 import logging
+from io import BytesIO
 
+import numpy
+import pandas as pd
 from aiohttp import ClientSession
 
 log = logging.getLogger(__name__)
@@ -23,3 +26,25 @@ async def get_bytes(url: str, session: ClientSession) -> bytes | None:
     except Exception as e:
         log.error(f"Error: Failed to fetch data from {url}: {e}")
         raise e
+
+
+def extract_data_from_file(data: bytes) -> pd.DataFrame | None:
+    """
+    Reads data from an XLS file and filter it based on certain conditions.
+
+    Args:
+        data (bytes): The XLS file data to read.
+
+    Returns:
+        pd.DataFrame: The filtered DataFrame or None if an error occurred.
+    """
+    file = pd.read_excel(BytesIO(data), index_col=False)
+    rows, cols = numpy.where(file == "Единица измерения: Метрическая тонна")
+    row = rows[0] + 2
+
+    filtered = pd.read_excel(
+        io=data,
+        skiprows=row,
+        usecols=[1, 2, 3, 4, 5, 14],
+    )
+    return filtered[filtered["Количество\nДоговоров,\nшт."] != "-"]
