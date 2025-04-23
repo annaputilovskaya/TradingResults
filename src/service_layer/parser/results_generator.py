d6b8ed5e4ed63b723ff38997479e6443c0314e52
anna putilovskaya <putilovskayaam@gmail.com>
2025-04-23 16:45:47 +0300

    create results_generator.py

---
+++ src/service_layer/parser/results_generator.py
@@ -0,0 +1,37 @@
+import pandas as pd
+
+from src import ORMTradingResult
+from src.models.domain import TradingResult
+from src.service_layer.utils import get_date_from_link
+
+
+def generate_trading_result_objects(data: pd.DataFrame, link: str):
+    """
+    Generates trading results objects from rows of DataFrame.
+
+    Args:
+        data (pd.DataFrame): DataFrame containing trading result data.
+        link (str): Link to the source data.
+
+    Yields:
+        ORMTradingResult: Trading result object in database.
+    """
+    for i in range(1, len(data) - 2):
+        row = data.iloc[i]
+        if (
+            pd.isna(row["Объем\nДоговоров\nв единицах\nизмерения"])
+            or row.apply(lambda x: "Итого" in str(x)).any()
+        ):
+            continue
+        else:
+            yield ORMTradingResult(
+                **TradingResult(
+                    exchange_product_id=row["Код\nИнструмента"],
+                    exchange_product_name=row["Наименование\nИнструмента"],
+                    delivery_basis_name=row["Базис\nпоставки"],
+                    volume=int(row["Объем\nДоговоров\nв единицах\nизмерения"]),
+                    total=int(row["Обьем\nДоговоров,\nруб."]),
+                    count=int(row["Количество\nДоговоров,\nшт."]),
+                    date=get_date_from_link(link),
+                ).to_dict()
+            )
