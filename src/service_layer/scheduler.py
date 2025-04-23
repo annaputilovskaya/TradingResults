ba089da7added75e55d8140b34fd3b1aed86187c
anna putilovskaya <putilovskayaam@gmail.com>
2025-04-23 23:11:13 +0300

    move main_parser function to created scheduler.py and add query to get the earliest date to function

---
+++ src/service_layer/scheduler.py
@@ -0,0 +1,32 @@
+import logging
+from time import time
+
+from aiohttp import ClientSession
+
+from src import dbh
+from src.service_layer.parser.data_parser import parse_trading_results
+from src.service_layer.parser.links_parser import get_new_trading_results_links
+from src.service_layer.queries import get_dates
+
+log = logging.getLogger(__name__)
+
+
+async def main_parser():
+    """
+    Main function to parse and save trading results.
+    """
+    async with dbh.session_factory() as db:
+        result = await get_dates(db=db, days=1)
+        if result:
+            earliest_date = result[0]
+        else:
+            earliest_date = "20230101"
+        log.info(f"Earliest date: {earliest_date}.")
+    t0 = time()
+    async with ClientSession() as session:
+        links = await get_new_trading_results_links(session, earliest_date)
+    if links:
+        await parse_trading_results(links)
+        log.info(f"Finished. Execution time {time() - t0:.3f} second")
+    else:
+        log.info("No new trading results found.")
