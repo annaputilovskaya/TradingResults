14dc3af5e2326bb86707a4e507a660bc51271e74
anna putilovskaya <putilovskayaam@gmail.com>
2025-04-24 12:06:38 +0300

    create validate_dates_interval function

--- src/service_layer/utils.py
+++ src/service_layer/utils.py
@@ -1,4 +1,7 @@
 import re
+from datetime import date
+
+from fastapi import HTTPException
 
 
 def get_date_from_link(link: str) -> str:
@@ -13,3 +16,33 @@ def get_date_from_link(link: str) -> str:
     """
     match = re.search(r"oil_xls_(\d{8})\d{6}", link)
     return match.group(1)
+
+
+def validate_dates_interval(
+    start_date: date,
+    end_date: date,
+) -> tuple[str | date, str]:
+    """
+    Validate start and end dates.
+
+    Args:
+        start_date (date | None): Beginning of the period for analysis of dynamics.
+        end_date (date | None): End of the period for analysis of dynamics.
+
+    Returns:
+        start_date (date | None): Validated beginning of the period for analysis of dynamics.
+        end_date (date | None): Validated end of the period for analysis of dynamics.
+    """
+    if start_date:
+        start_date = start_date.strftime("%Y%m%d")
+    if not start_date or start_date < "20230101":
+        start_date = "20230101"
+    if end_date:
+        end_date = end_date.strftime("%Y%m%d")
+    else:
+        end_date = date.today().strftime("%Y%m%d")
+    if start_date > end_date:
+        raise HTTPException(
+            status_code=400, detail="Start date should be less or equal to end date"
+        )
+    return start_date, end_date
