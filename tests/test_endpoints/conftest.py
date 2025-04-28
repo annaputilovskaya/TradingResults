da22a138fdaa89ba11f11633b054f5cd78b4ca52
anna putilovskaya <putilovskayaam@gmail.com>
2025-04-28 10:27:43 +0300

    create conftest.py

---
+++ tests/test_endpoints/conftest.py
@@ -0,0 +1,94 @@
+import logging
+
+import asyncio
+import pytest
+import pytest_asyncio
+
+from typing import Any, AsyncGenerator
+from contextlib import asynccontextmanager
+from asgi_lifespan import LifespanManager
+from fastapi import FastAPI
+from httpx import ASGITransport, AsyncClient
+from sqlalchemy import insert
+
+from src.config import settings
+from src.models import test_dbh, Base, ORMTradingResult
+from src.models.domain import TradingResult
+from src.routers import router
+
+test_db = test_dbh(
+    settings.db.test_url,
+)
+
+exchange_product_ids = ["A100ABS025A", "A100ANK060F", "A100NFT005A", "A100NVY060F", "A100STI060F"]
+dates = ["20230101", "20230102", "20230103", "20240101", "20250101"]
+test_data = [
+        TradingResult(
+            exchange_product_id=exchange_product_id,
+            exchange_product_name="Product",
+            delivery_basis_name="Basis",
+            volume=10,
+            total=100000,
+            count=1,
+            date=date
+        ).to_dict() for exchange_product_id in exchange_product_ids for date in dates
+]
+
+async def override_get_async_session():
+    """
+    Overrides dependency for getting asynchronous session for testing.
+
+    Yields:
+        AsyncSession: Asynchronous session for testing.
+    """
+    async with test_db.session_factory() as session:
+        yield session
+
+
+@pytest.fixture(scope='session', autouse=True)
+def event_loop():
+    """
+    Yields event loop for test session.
+    """
+    loop = asyncio.get_event_loop_policy().new_event_loop()
+    yield loop
+    loop.close()
+
+
+@pytest_asyncio.fixture(scope='session', autouse=True)
+async def app() -> AsyncGenerator[LifespanManager, Any]:
+    """
+    Create FastAPI application for testing.
+
+    Yields:
+         FastAPI application.
+    """
+    @asynccontextmanager
+    async def lifespan(app: FastAPI):
+        """
+        Prepare testing database for tests.
+        """
+        async with test_db.engine.begin() as conn:
+            await conn.run_sync(Base.metadata.drop_all)
+            await conn.run_sync(Base.metadata.create_all)
+            await conn.execute(insert(ORMTradingResult), test_data)
+            await conn.commit()
+        yield
+        async with test_db.engine.begin() as conn:
+            await conn.run_sync(Base.metadata.drop_all)
+
+    test_app = FastAPI(docs_url=None,
+                  redoc_url=None,
+                  lifespan=lifespan,
+                  )
+    test_app.include_router(router=router)
+    test_app.dependency_overrides[test_db.session_getter] = override_get_async_session
+
+    async with LifespanManager(test_app) as manager:
+        yield manager.app
+
+
+@pytest_asyncio.fixture(scope="session", autouse=True)
+async def get_async_session():
+    async with test_db.session_factory() as session:
+        yield session
