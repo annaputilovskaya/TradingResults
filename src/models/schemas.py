97f01b90d80871d7c9b8b8544a854b93d4b665d1
anna putilovskaya <putilovskayaam@gmail.com>
2025-04-28 20:01:22 +0300

    create schemas.py

---
+++ src/models/schemas.py
@@ -0,0 +1,17 @@
+from datetime import datetime
+
+from pydantic import BaseModel, Field
+
+
+class TradingResultSchema(BaseModel):
+    exchange_product_id: str = Field(max_length=11)
+    exchange_product_name: str
+    delivery_basis_name: str
+    volume: int
+    total: int
+    count: int
+    date: str = Field(max_length=8)
+    oil_id: str = Field(max_length=4)
+    delivery_basis_id: str = Field(3)
+    delivery_type_id: str= Field(1)
+
