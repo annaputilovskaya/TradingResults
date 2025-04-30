540524ed7d532a4dd003823959d4cdc17d1c6a63
anna putilovskaya <putilovskayaam@gmail.com>
2025-04-30 11:16:50 +0300

    create main conftest.py and move some fixtures, change scope for those fixtures

---
+++ tests/conftest.py
@@ -0,0 +1,85 @@
+import asyncio
+from contextlib import asynccontextmanager
+from typing import AsyncGenerator, Any
+
+import pytest
+import pytest_asyncio
+from asgi_lifespan import LifespanManager
+from fastapi import FastAPI
+from fastapi_cache import FastAPICache
+from fastapi_cache.backends.inmemory import InMemoryBackend
+
+from src.config import settings
+from src.models import test_dbh, Base
+from src.routers import router
+
+test_db = test_dbh(
+    settings.db.test_url,
+)
+
+
+async def override_get_async_session():
+    """
+    Overrides dependency for getting asynchronous session for testing.
+
+    Yields:
+        AsyncSession: Asynchronous session for testing.
+    """
+    async with test_db.session_factory() as session:
+        yield session
+
+
+@pytest.fixture(scope='session', autouse=True)
+def event_loop():
+    """
+    Yields event loop for test session.
+    """
+    loop = asyncio.get_event_loop_policy().new_event_loop()
+    yield loop
+    loop.close()
+
+
+@pytest_asyncio.fixture(scope='session', autouse=True)
+async def app() -> AsyncGenerator[LifespanManager, Any]:
+    """
+    Create FastAPI application for testing.
+
+    Yields:
+         FastAPI application.
+    """
+    @asynccontextmanager
+    async def lifespan(app: FastAPI):
+        """
+        Create testing database for tests.
+        """
+        FastAPICache.init(InMemoryBackend())
+        async with test_db.engine.begin() as conn:
+            await conn.run_sync(Base.metadata.drop_all)
+            await conn.run_sync(Base.metadata.create_all)
+        yield
+        async with test_db.engine.begin() as conn:
+            await conn.run_sync(Base.metadata.drop_all)
+        FastAPICache.reset()
+
+
+    test_app = FastAPI(docs_url=None,
+                  redoc_url=None,
+                  lifespan=lifespan,
+                  )
+    test_app.include_router(router=router)
+    test_app.dependency_overrides[test_db.session_getter] = override_get_async_session
+
+    async with LifespanManager(test_app) as manager:
+        yield manager.app
+
+
+@pytest_asyncio.fixture(scope="session", autouse=True)
+async def get_async_session():
+    """
+    Overrides dependency for getting asynchronous session for testing.
+
+    Yields:
+        AsyncSession: Asynchronous session for testing.
+    """
+    async with test_db.session_factory() as session:
+        yield session
--- tests/test_endpoints/conftest.py
+++ tests/test_endpoints/conftest.py
@@ -1,24 +1,15 @@
-import asyncio
 import pytest
 import pytest_asyncio
 
 from typing import Any, AsyncGenerator
-from contextlib import asynccontextmanager
-from asgi_lifespan import LifespanManager
 from fastapi import FastAPI
 from fastapi_cache import FastAPICache
-from fastapi_cache.backends.inmemory import InMemoryBackend
 from httpx import ASGITransport, AsyncClient
 from sqlalchemy import insert
 
-from src.config import settings
-from src.models import test_dbh, Base, ORMTradingResult
+from src.models import ORMTradingResult
 from src.models.domain import TradingResult
-from src.routers import router
-
-test_db = test_dbh(
-    settings.db.test_url,
-)
+from tests.conftest import app, test_db
 
 exchange_product_ids = ["A100ABS025A", "A100ANK060F", "A100NFT005A", "A100NVY060F", "A100STI060F"]
 dates = ["20230101", "20230102", "20230103", "20240101", "20250101"]
@@ -34,73 +25,15 @@ test_data = [
         ).to_dict() for exchange_product_id in exchange_product_ids for date in dates
 ]
 
-async def override_get_async_session():
-    """
-    Overrides dependency for getting asynchronous session for testing.
-
-    Yields:
-        AsyncSession: Asynchronous session for testing.
-    """
-    async with test_db.session_factory() as session:
-        yield session
-
-
-@pytest.fixture(scope='package', autouse=True)
-def event_loop():
-    """
-    Yields event loop for test session.
-    """
-    loop = asyncio.get_event_loop_policy().new_event_loop()
-    yield loop
-    loop.close()
-
-
 @pytest_asyncio.fixture(scope='package', autouse=True)
-async def app() -> AsyncGenerator[LifespanManager, Any]:
+async def prepare_database():
     """
-    Create FastAPI application for testing.
-
-    Yields:
-         FastAPI application.
+    Prepare testing database for tests.
     """
-    @asynccontextmanager
-    async def lifespan(app: FastAPI):
-        """
-        Prepare testing database for tests.
-        """
-        FastAPICache.init(InMemoryBackend())
-        async with test_db.engine.begin() as conn:
-            await conn.run_sync(Base.metadata.drop_all)
-            await conn.run_sync(Base.metadata.create_all)
-            await conn.execute(insert(ORMTradingResult), test_data)
-            await conn.commit()
-        yield
-        async with test_db.engine.begin() as conn:
-            await conn.run_sync(Base.metadata.drop_all)
-        FastAPICache.reset()
-
-
-    test_app = FastAPI(docs_url=None,
-                  redoc_url=None,
-                  lifespan=lifespan,
-                  )
-    test_app.include_router(router=router)
-    test_app.dependency_overrides[test_db.session_getter] = override_get_async_session
-
-    async with LifespanManager(test_app) as manager:
-        yield manager.app
-
-
-@pytest_asyncio.fixture(scope="package", autouse=True)
-async def get_async_session():
-    """
-    Overrides dependency for getting asynchronous session for testing.
-
-    Yields:
-        AsyncSession: Asynchronous session for testing.
-    """
-    async with test_db.session_factory() as session:
-        yield session
+    async with test_db.engine.begin() as conn:
+        await conn.execute(insert(ORMTradingResult), test_data)
+        await conn.commit()
+    yield
 
 
 @pytest_asyncio.fixture(scope='package', autouse=True)
@@ -123,4 +56,7 @@ async def client(app: FastAPI) -> AsyncGenerator[AsyncClient, Any]:
 
 @pytest.fixture(name="disable_cache", scope="class")
 async def in_memory_cache_clear():
+    """
+    Disable cache for testing.
+    """
     return await FastAPICache.clear()
